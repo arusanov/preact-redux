53ab0259175c62dac9e6d21e5ce77eb94f9ae5c9
arusanov <alexander.rusanov@gmail.com>
2017-12-07 16:13:50 +0300

    2.0.5

--- src/preact-redux.d.ts
+++ src/preact-redux.d.ts
@@ -92,32 +92,19 @@ export interface Connect {
     TOwnProps
   >
 
-<<<<<<< HEAD
-  export function connect<no_state, TDispatchProps, TOwnProps>(
-=======
   <no_state = {}, TDispatchProps = {}, TOwnProps = {}>(
->>>>>>> typescript-defs
     mapStateToProps: null | undefined,
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>
   ): InferableComponentEnhancerWithProps<TDispatchProps, TOwnProps>
 
-<<<<<<< HEAD
-  export function connect<TStateProps, TDispatchProps, TOwnProps>(
-    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
-=======
   <TStateProps = {}, TDispatchProps = {}, TOwnProps = {}, State = {}>(
     mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps, State>,
->>>>>>> typescript-defs
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>
   ): InferableComponentEnhancerWithProps<
     TStateProps & TDispatchProps,
     TOwnProps
   >
 
-<<<<<<< HEAD
-  export function connect<TStateProps, no_dispatch, TOwnProps, TMergedProps>(
-    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
-=======
   <
     TStateProps = {},
     no_dispatch = {},
@@ -126,35 +113,22 @@ export interface Connect {
     State = {}
   >(
     mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps, State>,
->>>>>>> typescript-defs
     mapDispatchToProps: null | undefined,
     mergeProps: MergeProps<TStateProps, undefined, TOwnProps, TMergedProps>
   ): InferableComponentEnhancerWithProps<TMergedProps, TOwnProps>
 
-<<<<<<< HEAD
-  export function connect<no_state, TDispatchProps, TOwnProps, TMergedProps>(
-=======
   <no_state = {}, TDispatchProps = {}, TOwnProps = {}, TMergedProps = {}>(
->>>>>>> typescript-defs
     mapStateToProps: null | undefined,
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
     mergeProps: MergeProps<undefined, TDispatchProps, TOwnProps, TMergedProps>
   ): InferableComponentEnhancerWithProps<TMergedProps, TOwnProps>
 
-<<<<<<< HEAD
-  export function connect<no_state, no_dispatch, TOwnProps, TMergedProps>(
-=======
   <no_state = {}, no_dispatch = {}, TOwnProps = {}, TMergedProps = {}>(
->>>>>>> typescript-defs
     mapStateToProps: null | undefined,
     mapDispatchToProps: null | undefined,
     mergeProps: MergeProps<undefined, undefined, TOwnProps, TMergedProps>
   ): InferableComponentEnhancerWithProps<TMergedProps, TOwnProps>
 
-<<<<<<< HEAD
-  export function connect<TStateProps, TDispatchProps, TOwnProps, TMergedProps>(
-    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
-=======
   <
     TStateProps = {},
     TDispatchProps = {},
@@ -163,43 +137,20 @@ export interface Connect {
     State = {}
   >(
     mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps, State>,
->>>>>>> typescript-defs
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
     mergeProps: MergeProps<TStateProps, TDispatchProps, TOwnProps, TMergedProps>
   ): InferableComponentEnhancerWithProps<TMergedProps, TOwnProps>
 
-<<<<<<< HEAD
-  export function connect<TStateProps, no_dispatch, TOwnProps>(
-    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
-    mapDispatchToProps: null | undefined,
-    mergeProps: null | undefined,
-    options: Options<TStateProps, TOwnProps>
-=======
   <TStateProps = {}, no_dispatch = {}, TOwnProps = {}, State = {}>(
     mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps, State>,
     mapDispatchToProps: null | undefined,
     mergeProps: null | undefined,
     options: Options<State, TStateProps, TOwnProps>
->>>>>>> typescript-defs
   ): InferableComponentEnhancerWithProps<
     DispatchProp<any> & TStateProps,
     TOwnProps
   >
 
-<<<<<<< HEAD
-  export function connect<no_state, TDispatchProps, TOwnProps>(
-    mapStateToProps: null | undefined,
-    mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
-    mergeProps: null | undefined,
-    options: Options<no_state, TOwnProps>
-  ): InferableComponentEnhancerWithProps<TDispatchProps, TOwnProps>
-
-  export function connect<TStateProps, TDispatchProps, TOwnProps>(
-    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
-    mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
-    mergeProps: null | undefined,
-    options: Options<TStateProps, TOwnProps>
-=======
   <TStateProps = {}, TDispatchProps = {}, TOwnProps = {}>(
     mapStateToProps: null | undefined,
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
@@ -212,16 +163,11 @@ export interface Connect {
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
     mergeProps: null | undefined,
     options: Options<State, TStateProps, TOwnProps>
->>>>>>> typescript-defs
   ): InferableComponentEnhancerWithProps<
     TStateProps & TDispatchProps,
     TOwnProps
   >
 
-<<<<<<< HEAD
-  export function connect<TStateProps, TDispatchProps, TOwnProps, TMergedProps>(
-    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
-=======
   <
     TStateProps = {},
     TDispatchProps = {},
@@ -230,7 +176,6 @@ export interface Connect {
     State = {}
   >(
     mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps, State>,
->>>>>>> typescript-defs
     mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
     mergeProps: MergeProps<
       TStateProps,
@@ -238,192 +183,6 @@ export interface Connect {
       TOwnProps,
       TMergedProps
     >,
-<<<<<<< HEAD
-    options: Options<TStateProps, TOwnProps, TMergedProps>
-  ): InferableComponentEnhancerWithProps<TMergedProps, TOwnProps>
-
-  interface MapStateToProps<TStateProps, TOwnProps> {
-    (state: any, ownProps: TOwnProps): TStateProps
-  }
-
-  interface MapStateToPropsFactory<TStateProps, TOwnProps> {
-    (initialState: any, ownProps: TOwnProps): MapStateToProps<
-      TStateProps,
-      TOwnProps
-    >
-  }
-
-  type MapStateToPropsParam<TStateProps, TOwnProps> =
-    | MapStateToProps<TStateProps, TOwnProps>
-    | MapStateToPropsFactory<TStateProps, TOwnProps>
-
-  interface MapDispatchToPropsFunction<TDispatchProps, TOwnProps> {
-    (dispatch: Dispatch<any>, ownProps: TOwnProps): TDispatchProps
-  }
-
-  type MapDispatchToProps<TDispatchProps, TOwnProps> =
-    | MapDispatchToPropsFunction<TDispatchProps, TOwnProps>
-    | TDispatchProps
-
-  interface MapDispatchToPropsFactory<TDispatchProps, TOwnProps> {
-    (dispatch: Dispatch<any>, ownProps: TOwnProps): MapDispatchToProps<
-      TDispatchProps,
-      TOwnProps
-    >
-  }
-
-  type MapDispatchToPropsParam<TDispatchProps, TOwnProps> =
-    | MapDispatchToProps<TDispatchProps, TOwnProps>
-    | MapDispatchToPropsFactory<TDispatchProps, TOwnProps>
-
-  interface MergeProps<TStateProps, TDispatchProps, TOwnProps, TMergedProps> {
-    (
-      stateProps: TStateProps,
-      dispatchProps: TDispatchProps,
-      ownProps: TOwnProps
-    ): TMergedProps
-  }
-
-  interface Options<TStateProps = {}, TOwnProps = {}, TMergedProps = {}>
-    extends ConnectOptions {
-    /**
-     * If true, implements shouldComponentUpdate and shallowly compares the result of mergeProps,
-     * preventing unnecessary updates, assuming that the component is a “pure” component
-     * and does not rely on any input or state other than its props and the selected Redux store’s state.
-     * Defaults to true.
-     * @default true
-     */
-    pure?: boolean
-
-    /**
-     * When pure, compares incoming store state to its previous value.
-     * @default strictEqual
-     */
-    areStatesEqual?: (nextState: any, prevState: any) => boolean
-
-    /**
-     * When pure, compares incoming props to its previous value.
-     * @default shallowEqual
-     */
-    areOwnPropsEqual?: (
-      nextOwnProps: TOwnProps,
-      prevOwnProps: TOwnProps
-    ) => boolean
-
-    /**
-     * When pure, compares the result of mapStateToProps to its previous value.
-     * @default shallowEqual
-     */
-    areStatePropsEqual?: (
-      nextStateProps: TStateProps,
-      prevStateProps: TStateProps
-    ) => boolean
-
-    /**
-     * When pure, compares the result of mergeProps to its previous value.
-     * @default shallowEqual
-     */
-    areMergedPropsEqual?: (
-      nextMergedProps: TMergedProps,
-      prevMergedProps: TMergedProps
-    ) => boolean
-  }
-
-  /**
-   * Connects a React component to a Redux store. It is the base for {@link connect} but is less opinionated about
-   * how to combine <code>state</code>, <code>props</code>, and <code>dispatch</code> into your final props. It makes no
-   * assumptions about defaults or memoization of results, leaving those responsibilities to the caller.It does not
-   * modify the component class passed to it; instead, it returns a new, connected component class for you to use.
-   *
-   * @param selectorFactory The selector factory. See {@type SelectorFactory} for details.
-   * @param connectOptions If specified, further customizes the behavior of the connector. Additionally, any extra
-   *     options will be passed through to your <code>selectorFactory</code> in the <code>factoryOptions</code> argument.
-   */
-  export function connectAdvanced<S, TProps, TOwnProps, TFactoryOptions = {}>(
-    selectorFactory: SelectorFactory<S, TProps, TOwnProps, TFactoryOptions>,
-    connectOptions?: ConnectOptions & TFactoryOptions
-  ): AdvancedComponentDecorator<TProps, TOwnProps>
-
-  /**
-   * Initializes a selector function (during each instance's constructor). That selector function is called any time the
-   * connector component needs to compute new props, as a result of a store state change or receiving new props. The
-   * result of <code>selector</code> is expected to be a plain object, which is passed as the props to the wrapped
-   * component. If a consecutive call to <code>selector</code> returns the same object (<code>===</code>) as its previous
-   * call, the component will not be re-rendered. It's the responsibility of <code>selector</code> to return that
-   * previous object when appropriate.
-   */
-  export interface SelectorFactory<S, TProps, TOwnProps, TFactoryOptions> {
-    (dispatch: Dispatch<S>, factoryOptions: TFactoryOptions): Selector<
-      S,
-      TProps,
-      TOwnProps
-    >
-  }
-
-  export interface Selector<S, TProps, TOwnProps> {
-    (state: S, ownProps: TOwnProps): TProps
-  }
-
-  export interface ConnectOptions {
-    /**
-     * Computes the connector component's displayName property relative to that of the wrapped component. Usually
-     * overridden by wrapper functions.
-     *
-     * @default name => 'ConnectAdvanced('+name+')'
-     * @param componentName
-     */
-    getDisplayName?: (componentName: string) => string
-    /**
-     * Shown in error messages. Usually overridden by wrapper functions.
-     *
-     * @default 'connectAdvanced'
-     */
-    methodName?: string
-    /**
-     * If defined, a property named this value will be added to the props passed to the wrapped component. Its value
-     * will be the number of times the component has been rendered, which can be useful for tracking down unnecessary
-     * re-renders.
-     *
-     * @default undefined
-     */
-    renderCountProp?: string
-    /**
-     * Controls whether the connector component subscribes to redux store state changes. If set to false, it will only
-     * re-render on <code>componentWillReceiveProps</code>.
-     *
-     * @default true
-     */
-    shouldHandleStateChanges?: boolean
-    /**
-     * The key of props/context to get the store. You probably only need this if you are in the inadvisable position of
-     * having multiple stores.
-     *
-     * @default 'store'
-     */
-    storeKey?: string
-    /**
-     * If true, stores a ref to the wrapped component instance and makes it available via getWrappedInstance() method.
-     *
-     * @default false
-     */
-    withRef?: boolean
-  }
-
-  export interface ProviderProps {
-    /**
-     * The single Redux store in your application.
-     */
-    store?: Store<any>
-    children?: VNode
-  }
-
-  /**
-   * Makes the Redux store available to the connect() calls in the component hierarchy below.
-   */
-  export class Provider extends Component<ProviderProps, {}> {
-    render(props?: ProviderProps): VNode
-  }
-=======
     options: Options<State, TStateProps, TOwnProps, TMergedProps>
   ): InferableComponentEnhancerWithProps<TMergedProps, TOwnProps>
 }
@@ -625,7 +384,6 @@ export interface ProviderProps {
  */
 export class Provider extends Preact.Component<ProviderProps, {}> {
   render(): ReactNode
->>>>>>> typescript-defs
 }
 
 /**
